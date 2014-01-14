2f27b3980f2d33a8d612fcce91bff9910c6646d6
Soyoon <soyoon0918@nate.com>
2014-01-14 22:04:34 +0900

    인트로 레이아웃 및 시작화면 배경 구성

---
+++ src/com/nexters/vobble/IntroActivity.java
@@ -0,0 +1,27 @@
+package com.nexters.vobble;
+
+import android.app.*;
+import android.content.*;
+import android.os.*;
+
+public class IntroActivity extends Activity {
+	private Handler handler = null;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_intro);
+		
+		handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			public void run() {
+				endIntro();
+			}
+		}, 500);		
+	}
+
+	private void endIntro() {
+		Intent intent = new Intent(this, StartActivity.class);
+		startActivity(intent);
+		finish();
+	}	
+}
---
+++ src/com/nexters/vobble/StartActivity.java
@@ -0,0 +1,14 @@
+package com.nexters.vobble;
+
+import android.app.*;
+import android.os.*;
+
+public class StartActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start);
+
+	}
+}
