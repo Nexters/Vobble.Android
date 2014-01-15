5a7ae9eae76b998b0b3f6196dce5448a7d7bbfcf
Soyoon <soyoon0918@nate.com>
2014-01-16 02:49:19 +0900

    메인 화면 레이아웃 잡는중

--- src/com/nexters/vobble/LoginActivity.java
+++ src/com/nexters/vobble/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.nexters.vobble;
 
 import android.app.*;
+import android.content.*;
 import android.os.*;
+import android.view.*;
+import android.widget.*;
 
 public class LoginActivity extends Activity {
-
+	private ImageView loginImageView = null;
+	private Intent intent = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		loginImageView = (ImageView) findViewById(R.id.login_check);
+		
+		loginImageView.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				intent = new Intent(LoginActivity.this, MainActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
--- src/com/nexters/vobble/MainActivity.java
+++ src/com/nexters/vobble/MainActivity.java
@@ -2,10 +2,11 @@ package com.nexters.vobble;
 
 import android.app.*;
 import android.os.*;
+import android.support.v4.view.*;
+import android.widget.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
 
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
