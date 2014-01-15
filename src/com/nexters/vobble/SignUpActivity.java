54aba3c9ee8681950792e88213e597b434c43f72
Soyoon <soyoon0918@nate.com>
2014-01-15 14:50:28 +0900

    회원가입 레이아웃 완료 및 시작화면에서 액티비티넘어가기

---
+++ src/com/nexters/vobble/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.nexters.vobble;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+
+public class SignUpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_signup);
+	}
+}
--- src/com/nexters/vobble/StartActivity.java
+++ src/com/nexters/vobble/StartActivity.java
@@ -1,14 +1,40 @@
 package com.nexters.vobble;
 
 import android.app.*;
+import android.content.*;
 import android.os.*;
+import android.view.*;
+import android.widget.*;
 
 public class StartActivity extends Activity {
-
+	private ImageView loginImageView = null;
+	private ImageView signUpImageView = null;
+	private Intent intent = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_start);
-
+		
+		loginImageView = (ImageView) findViewById(R.id.loginBtn);
+		signUpImageView = (ImageView) findViewById(R.id.signUpBtn);
+		
+		loginImageView.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View arg0) {
+				intent = new Intent(StartActivity.this, LoginActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		signUpImageView.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				intent  = new Intent(StartActivity.this, SignUpActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		});
 	}
+	
+	
 }
