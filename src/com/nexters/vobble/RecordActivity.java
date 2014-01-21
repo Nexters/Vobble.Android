875fcde53aea4501ecf27e7df3f2ebd4524f6297
Soyoon <soyoon0918@nate.com>
2014-01-21 20:45:57 +0900

    맵 레이아웃 만드는중

--- src/com/nexters/vobble/RecordActivity.java
+++ src/com/nexters/vobble/RecordActivity.java
@@ -2,15 +2,27 @@ package com.nexters.vobble;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.*;
+import android.view.*;
+import android.widget.*;
 import android.support.v4.app.NavUtils;
 
 public class RecordActivity extends Activity {
-
+	private ImageView confirmImageView = null;
+	private Intent intent = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_record);
+		
+		confirmImageView = (ImageView) findViewById(R.id.record_confirm_btn);
+		
+		confirmImageView.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				intent = new Intent(RecordActivity.this, MapActivity.class);
+				startActivity(intent);
+			}
+		});
+		
 	}
 }
