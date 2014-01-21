76605c7b5f6bb9725d537540e574628b58114a5a
Soyoon <soyoon0918@nate.com>
2014-01-21 20:25:19 +0900

    메인이랑 녹음레이아웃완성

--- src/com/nexters/vobble/MainActivity.java
+++ src/com/nexters/vobble/MainActivity.java
@@ -1,14 +1,27 @@
 package com.nexters.vobble;
 
 import android.app.*;
+import android.content.*;
 import android.os.*;
 import android.support.v4.view.*;
+import android.view.*;
 import android.widget.*;
 
 public class MainActivity extends Activity{
-
+	private ImageView recordImageView = null;
+	private Intent intent = null;
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		recordImageView = (ImageView) findViewById(R.id.voice_record_btn);
+		
+		recordImageView.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				intent = new Intent(MainActivity.this, RecordActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
