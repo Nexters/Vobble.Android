7fcc2056d01b6b57659b917df0a2e52333bab3cb
Soyoon <soyoon0918@nate.com>
2014-02-06 16:38:45 +0900

    레코드 액티비티 기본함수

--- src/com/nexters/vobble/RecordActivity.java
+++ src/com/nexters/vobble/RecordActivity.java
@@ -8,49 +8,46 @@ import android.widget.*;
 import android.support.v4.app.NavUtils;
 
 public class RecordActivity extends Activity implements View.OnClickListener{
-	private ImageView confirmImageView;
-	private Intent intent;
+	private Button confirmBtn;
+	private ImageView pauseImageView;
+	private ImageView resetImageView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_record);
 		
-		
-		
-	
-		confirmImageView.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				intent = new Intent(RecordActivity.this, MapActivity.class);
-				startActivity(intent);
-			}
-		});
+		initResource();
+		initEvent();
 	}
 	
 	private void initResource(){
-		confirmImageView = (ImageView) findViewById(R.id.record_confirm_btn);
-	
+		confirmBtn = (Button) findViewById(R.id.btn_record_confirm);
+		pauseImageView = (ImageView)findViewById(R.id.iv_record_pause_btn);
+		resetImageView = (ImageView)findViewById(R.id.iv_record_re_btn);
 	}
 	
 	private void initEvent(){
-		
+		confirmBtn.setOnClickListener(this);
+		pauseImageView.setOnClickListener(this);
+		resetImageView.setOnClickListener(this);
 	}
 	
 	@Override
 	public void onClick(View view){
+		Intent intent = null;
 		switch(view.getId()){
-		case R.id.iv_record_camera_icon : 
-			intent = new Intent(RecordActivity.this, MapActivity.class);
-			startActivity(intent);
+		case R.id.iv_record_camera_icon :
 			break;
 		case R.id.iv_record_pause_btn :
 			break;
 		case R.id.iv_record_re_btn : 
 			break;
 		case R.id.btn_record_confirm :
+			intent = new Intent(RecordActivity.this, MapActivity.class);
+			startActivity(intent);
 			break;
 		}
-		
-		
 	}
 }
