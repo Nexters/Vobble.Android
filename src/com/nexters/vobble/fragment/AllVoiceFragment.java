c68eca2b36745e4a2ac50a4756c9b184cec88890
blaswan <lsw9549@gmail.com>
2014-02-06 19:52:41 +0900

    메인화면에서 모든 보블 갯수, 내 보블 갯수 초기화 코두 추가

--- src/com/nexters/vobble/fragment/AllVoiceFragment.java
+++ src/com/nexters/vobble/fragment/AllVoiceFragment.java
@@ -4,13 +4,54 @@ import android.os.*;
 import android.support.v4.app.Fragment;
 import android.view.*;
 
+import android.widget.TextView;
 import com.nexters.vobble.*;
+import com.nexters.vobble.core.ServerAPIRequest;
 
 public class AllVoiceFragment extends Fragment {
 	private View view;
+    private TextView tvAllVobblesCount;
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		view = inflater.inflate(R.layout.fragment_all_voice, null);
-		return view;
+		view = inflater.inflate(R.layout.fragment_all_vobbles, null);
+
+        initResources(view);
+        initVobblesCount();
+        return view;
 	}
+
+    private void initResources(View view) {
+        tvAllVobblesCount = (TextView) view.findViewById(R.id.tv_all_vobbles_count);
+    }
+
+    private void initVobblesCount() {
+        AllVobblesCountAsyncTask allVobblesCountAsyncTask = new AllVobblesCountAsyncTask();
+        allVobblesCountAsyncTask.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
+    }
+
+    private void setVobblesCount(Integer count) {
+        tvAllVobblesCount.setText(count + "");
+    }
+
+    private class AllVobblesCountAsyncTask extends AsyncTask<Integer, Integer, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... integers) {
+            int count = ServerAPIRequest.getAllVobblesCount();
+            return count;
+        }
+
+        @Override
+        protected void onPostExecute(Integer count) {
+            if (count >= 0) {
+                setVobblesCount(count);
+            }
+            super.onPostExecute(count);
+        }
+    }
 }
--- src/com/nexters/vobble/fragment/MyVoiceFragment.java
+++ src/com/nexters/vobble/fragment/MyVoiceFragment.java
@@ -5,13 +5,53 @@ import android.os.*;
 import android.support.v4.app.*;
 import android.view.*;
 
+import android.widget.TextView;
 import com.nexters.vobble.*;
 
 public class MyVoiceFragment extends Fragment{
 	private View view;
+    private TextView tvMyVobblesCount;
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		view = inflater.inflate(R.layout.fragment_my_voice, null);
-		return view;
+		view = inflater.inflate(R.layout.fragment_my_vobbles, null);
+		
+        initResources(view);
+        initVobblesCount();
+        return view;
 	}
+
+    private void initResources(View view) {
+        tvMyVobblesCount = (TextView) view.findViewById(R.id.tv_my_vobbles_count);
+    }
+
+    private void initVobblesCount() {
+        MyVobblesCountAsyncTask myVobblesCountAsyncTask = new MyVobblesCountAsyncTask();
+        myVobblesCountAsyncTask.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
+    }
+
+    private void setVobblesCount(Integer count) {
+        tvMyVobblesCount.setText(count + "");
+    }
+
+    private class MyVobblesCountAsyncTask extends AsyncTask<Integer, Integer, Integer> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... integers) {
+            // TODO: User 모델 만들면 userId 받아서 넘겨줘야 함
+            //int count = ServerAPIRequest.getMyVobblesCount();
+            return 0;
+        }
+
+        @Override
+        protected void onPostExecute(Integer count) {
+            if (count >= 0) {
+                setVobblesCount(count);
+            }
+            super.onPostExecute(count);
+        }
+    }
 }
