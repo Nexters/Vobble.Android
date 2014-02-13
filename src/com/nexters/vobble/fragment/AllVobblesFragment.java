1dfbc8216ad1261a2437bd9ebd87ccbef5a4c19c
blaswan <lsw9549@gmail.com>
2014-02-13 20:13:36 +0900

    API URL을 새로운 테스트 서버로 변경하고 보블 갯수 보여주는 부분 삭제

--- src/com/nexters/vobble/fragment/AllVobblesFragment.java
+++ src/com/nexters/vobble/fragment/AllVobblesFragment.java
@@ -4,7 +4,6 @@ import org.json.*;
 
 import android.os.*;
 import android.view.*;
-import android.widget.*;
 
 import com.loopj.android.http.*;
 import com.nexters.vobble.*;
@@ -13,8 +12,7 @@ import com.nexters.vobble.network.*;
 
 public class AllVobblesFragment extends BaseFragment {
 	private View view;
-    private TextView tvAllVobblesCount;
-    
+
     @Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,37 +22,12 @@ public class AllVobblesFragment extends BaseFragment {
 		view = inflater.inflate(R.layout.fragment_all_vobbles, null);
 
         initResources(view);
-        initVobblesCount();
         initVobbles();
         return view;
 	}
 
     private void initResources(View view) {
-        tvAllVobblesCount = (TextView) view.findViewById(R.id.tv_all_vobbles_count);
-    }
 
-    private void initVobblesCount() {
-    	String url = URL.VOBBLES_COUNT;
-		HttpUtil.get(url, null, null, new VobbleResponseHandler(activity) {
-			
-			@Override
-			public void onStart() {
-				super.onStart();
-				showLoading();
-			}
-
-			@Override
-			public void onFinish() {
-				super.onFinish();
-				hideLoading();
-			}
-
-			@Override
-			public void onSuccess(JSONObject response) {
-				String count = response.optString("count");
-				setVobblesCount(Integer.parseInt(count));
-			}
-		});
     }
 
     private void initVobbles() {
@@ -84,9 +57,5 @@ public class AllVobblesFragment extends BaseFragment {
 
 			}
 		});
-    }
-    
-    private void setVobblesCount(Integer count) {
-        tvAllVobblesCount.setText(count + "");
     }
 }
--- src/com/nexters/vobble/fragment/MyVobblesFragment.java
+++ src/com/nexters/vobble/fragment/MyVobblesFragment.java
@@ -1,22 +1,12 @@
 package com.nexters.vobble.fragment;
 
-
-import org.json.JSONObject;
-
 import android.os.*;
-import android.support.v4.app.*;
 import android.view.*;
 
-import android.widget.TextView;
-
-import com.loopj.android.http.RequestParams;
 import com.nexters.vobble.*;
-import com.nexters.vobble.core.*;
-import com.nexters.vobble.network.*;
 
 public class MyVobblesFragment extends BaseFragment{
 	private View view;
-    private TextView tvMyVobblesCount;
 
     @Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,49 +18,10 @@ public class MyVobblesFragment extends BaseFragment{
 		view = inflater.inflate(R.layout.fragment_my_vobbles, null);
 		
         initResources(view);
-        initVobblesCount();
         return view;
 	}
 
     private void initResources(View view) {
-        tvMyVobblesCount = (TextView) view.findViewById(R.id.tv_my_vobbles_count);
-    }
 
-    private void initVobblesCount() {
-    	String url = String.format(URL.USER_VOBBLES_COUNT,Vobble.getUserId(activity));
-    	
-		RequestParams params = new RequestParams();
-		params.put(Vobble.LATITUDE, "127");
-		params.put(Vobble.LONGITUDE, "37");
-		params.put(Vobble.LIMIT, "6");
-    	
-		HttpUtil.get(url, null, null, new VobbleResponseHandler(activity) {
-			
-			@Override
-			public void onStart() {
-				super.onStart();
-				showLoading();
-			}
-
-			@Override
-			public void onFinish() {
-				super.onFinish();
-				hideLoading();
-			}
-
-			@Override
-			public void onSuccess(JSONObject response) {
-				String count = response.optString("count");
-				setVobblesCount(Integer.parseInt(count));
-			}
-		});
-        /*
-    	MyVobblesCountAsyncTask myVobblesCountAsyncTask = new MyVobblesCountAsyncTask();
-        myVobblesCountAsyncTask.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
-        */
-    }
-
-    private void setVobblesCount(Integer count) {
-        tvMyVobblesCount.setText(count + "");
     }
 }
